e36917de143c05a610b36997156c4eaa29e1e50c
Moritz <101179677+Moritz-Deiaco@users.noreply.github.com>
2024-04-08 17:58:52 +0200

    Added CompletedSurveys DB row for user

---
+++ Pollifyr/App/Database/Migrations/20240408154649_User_1.0.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pollifyr.App.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class User_10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompletedSurveys",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedSurveys",
+                table: "Users");
+        }
+    }
+}
--- Pollifyr/App/Database/Models/User.cs
+++ Pollifyr/App/Database/Models/User.cs
@@ -7,6 +7,8 @@ public class User
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
 
+    public string CompletedSurveys { get; set; } = "";
+    
     public bool Admin { get; set; } = false;
     public DateTime TokenValidTimestamp { get; set; } = DateTime.UtcNow.AddMinutes(-5);
     
