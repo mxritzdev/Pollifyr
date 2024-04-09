6cef7fd69e8acc26d18db8b6daf939c49e454519
Moritz <101179677+Moritz-Deiaco@users.noreply.github.com>
2024-04-09 20:41:31 +0200

    Added Sorting id for sorting the answers

---
+++ Pollifyr/App/Database/Migrations/20240409180945_AddSortingRowAnswer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pollifyr.App.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSortingRowAnswer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SortingId",
+                table: "Answers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortingId",
+                table: "Answers");
+        }
+    }
+}
