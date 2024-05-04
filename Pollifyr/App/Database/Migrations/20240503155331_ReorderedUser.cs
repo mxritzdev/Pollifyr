a23a1a3ae3c65e741f1fedd4e7e973c19cbca00d
Moritz <101179677+Moritz-Deiaco@users.noreply.github.com>
2024-05-04 09:47:21 +0200

    Some more improvements

---
+++ Pollifyr/App/Database/Migrations/20240503155331_ReorderedUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pollifyr.App.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReorderedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
